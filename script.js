f7dcafcc6868f7fcc0c0bdc3cf8e2ff8e93afb01
Ilya <ilya.saenko.06@mail.ru>
2025-02-20 12:21:53 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+﻿document.getElementById('vpnEncryptionSlider').addEventListener('input', function () {
+            document.getElementById('vpnEncryptionValue').innerText = 'Шифрование: ' + this.value + '-bit';
+        });
+
+        // Валидация формы
+        (function () {
+            'use strict'
+
+            // Получаем все формы, к которым мы хотим применить пользовательскую логику валидации Bootstrap
+            var forms = document.querySelectorAll('.needs-validation')
+
+            // Loop over them and prevent submission
+            Array.prototype.slice.call(forms)
+                .forEach(function (form) {
+                    form.addEventListener('submit', function (event) {
+                        if (!form.checkValidity()) {
+                            event.preventDefault()
+                            event.stopPropagation()
+                        }
+
+                        form.classList.add('was-validated')
+                    }, false)
+                })
+        })()
